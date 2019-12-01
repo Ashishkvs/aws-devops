69ed7a8a78182e80664249cb0470481213b49501
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2019-12-01 07:42:24 +0530

    intial commit

---
+++ src/main/java/com/imagegrafia/AwsDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.imagegrafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDevopsApplication.class, args);
+	}
+
+}
